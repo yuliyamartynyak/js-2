a67e968b61bfb92ce28fa92d383d25222e234cec
Yuliya Martynyak <yuliya.martynyak@gmail.com>
2019-02-02 19:27:35 +0000

    on fire

--- js/index.js
+++ js/index.js
@@ -8,8 +8,25 @@
 //    i++;
 //}
 
-let i = 0;
-do {
-    console.log(i);
-    i++;
-} while (i <= 100)
+//let i = 0;
+//do {
+//    console.log(i);
+//    i++;
+//} while (i <= 100)
+
+let productNames = [];
+productNames.push('Tesla X');
+productNames.push('Chevrolet');
+productNames.push('Prado');
+console.log(productNames[2]);
+productNames[0] = 'Tesla S';
+console.log(productNames[0]);
+productNames[3] = 'ZAZ Lanos';
+console.log(productNames.length)
+
+console.log('Prinnting all');
+
+for (let i = 0; i < productNames.length; i++)
+console.log(productNames[i])
+
+
