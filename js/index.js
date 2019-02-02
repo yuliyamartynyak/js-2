eb7212ab99993c94fcf0fb728569acb802ae45e1
Yuliya Martynyak <yuliya.martynyak@gmail.com>
2019-02-02 17:51:41 +0000

    On fire

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//for (let i = 100; i >= 1; i = i - 1) {
+//  console.log(i);  
+//}
+
+//let i = 0;
+//while (i <= 100) {
+//    console.log(i);
+//    i++;
+//}
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 100)
